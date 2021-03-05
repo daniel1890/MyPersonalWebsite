1a2d723266e15e792b6cb52c61f2b6b2b04136d9
daniel1890 <danielroth1890@gmail.com>
2021-03-05 15:40:05 +0100

    add jsonprojectservice

---
+++ MyFirstWebsite/Services/JsonFileProjectService.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using Microsoft.AspNetCore.Hosting;
+using MyFirstWebsite.Models;
+
+namespace MyFirstWebsite.Services
+{
+    public class JsonFileProjectService
+    {
+        public JsonFileProjectService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "projects.json"); }
+        }
+
+        public IEnumerable<Project> GetProjects()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Project[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
